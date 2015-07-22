c5804142b10c2d2eaaaa6881deb0676154003708
Eric Vallee <eric_vallee2003@yahoo.ca>
2015-07-22 00:14:39 -0400

    Prepared environment to start writing tests.

--- test/Tests.js
+++ test/Tests.js
@@ -111,55 +111,187 @@ function Setup(UserSchema, Callback)
     });
 }
 
-var UserSchema = UserProperties({'Username': {
-                  'Required': true,
-                  'Unique': true,
-                  'Mutable': false,
-                  'Description': function(Value) {return UsernameRegex.test(Value)}
-              },
-              'Email': {
-                  'Required': true,
-                  'Unique': true,
-                  'Privacy': UserProperties.Privacy.Private,
-                  'Description': function(Value) {return EmailRegex.test(Value)}
-              },
-              'Password': {
-                  'Required': true,
-                  'Privacy': UserProperties.Privacy.Secret,
-                  'Retrievable': false,
-                  'Description': function(Value) {return PasswordRegex.test(Value)},
-                  'Sources': ['User', 'Auto'],
-                  'Generator': function(Callback) {Callback(null, Uid(15));}
-              },
-              'Gender': {
-                  'Privacy': UserProperties.Privacy.Private,
-                  'Mutable': false,
-                  'Description': function(Value) {return Value=='M'||Value=='F'} //Reality is more complex, but for the sake of this example...
-              },
-              'Age': {
-                  'Privacy': UserProperties.Privacy.Private,
-                  'Description': function(Value) {return typeof(Value)==typeof(1) && Value > 0}
-              },
-              'Address': {
-                  'Required': true,
-                  'Privacy': UserProperties.Privacy.Private
-              },
-              'EmailToken': {
-                  'Required': true,
-                  'Privacy': UserProperties.Privacy.Secret,
-                  'Access': 'Email',
-                  'Sources': ['Auto'],
-                  'Generator': function(Callback) {Callback(null, Uid(20));},
-                  'Description': EmailTokenValidation
-              },
-              '_id': {
-                  'Privacy': UserProperties.Privacy.Private,
-                  'Access': 'System',
-                  'Sources': ['MongoDB']
-              }});
-              
-Setup(UserSchema, function() {
-    console.log('Tests not yet implemented. Ran test server setup successfully. Will now exit...');
-    Context.Server.close();
-    Context.DB.close();
-});
+function RequestHandler()
+{
+    this.SessionID = null;
+    if(!RequestHandler.prototype.SetSessionID)
+    {
+        RequestHandler.prototype.SetSessionID = function(Headers) {
+            if(Headers["set-cookie"])
+            {
+                var SessionCookie = Headers["set-cookie"][0];
+                SessionCookie = SessionCookie.slice(String("connect.sid=").length, SessionCookie.indexOf(';'));
+                this.SessionID = SessionCookie;
+            }
+        };
+        
+        RequestHandler.prototype.Request = function(Method, Path, Callback, ReqBody, GetBody) {
+            var Self = this;
+            var RequestObject = {'hostname': 'localhost', 'port': 8080, 'method': Method, 'path': Path, 'headers': {'Accept': 'application/json'}};
+            if(this.SessionID)
+            {
+                RequestObject['headers']['cookie'] = 'connect.sid='+this.SessionID;
+            }
+            if(ReqBody)
+            {
+                RequestObject.headers['Content-Type']='application/json';
+                RequestObject.headers['Content-Length']=(JSON.stringify(ReqBody)).length;
+            }
+            var Req = Http.request(RequestObject, function(Res) {
+                Res.setEncoding('utf8');
+                var Body = "";
+                if(!GetBody)
+                {
+                    Res.resume();
+                }
+                else
+                {
+                    Res.on('data', function (Chunk) {
+                        Body+=Chunk;
+                    });
+                }
+                Res.on('end', function() {
+                    Self.SetSessionID(Res.headers);
+                    Body = GetBody && Body ? JSON.parse(Body) : null;
+                    Callback(Res.statusCode, Body);
+                });
+            });
+            if(ReqBody)
+            {
+                Req.write(JSON.stringify(ReqBody), function() {
+                    Req.end();
+                });
+            }
+            else
+            {
+                Req.end();
+            }
+        };
+    }
+}
+
+exports.Main = {
+    'setUp': function(Callback) {
+        var UserSchema = UserProperties(
+            {'Username': {
+              'Required': true,
+              'Unique': true,
+              'Mutable': false,
+              'Description': function(Value) {return UsernameRegex.test(Value)}
+            },
+            'Email': {
+              'Required': true,
+              'Unique': true,
+              'Privacy': UserProperties.Privacy.Private,
+              'Description': function(Value) {return EmailRegex.test(Value)}
+            },
+            'Password': {
+              'Required': true,
+              'Privacy': UserProperties.Privacy.Secret,
+              'Retrievable': false,
+              'Description': function(Value) {return PasswordRegex.test(Value)},
+              'Sources': ['User', 'Auto'],
+              'Generator': function(Callback) {Callback(null, Uid(15));}
+            },
+            'Gender': {
+              'Privacy': UserProperties.Privacy.Private,
+              'Mutable': false,
+              'Description': function(Value) {return Value=='M'||Value=='F'} //Reality is more complex, but for the sake of this example...
+            },
+            'Age': {
+              'Privacy': UserProperties.Privacy.Private,
+              'Description': function(Value) {return typeof(Value)==typeof(1) && Value > 0}
+            },
+            'Address': {
+              'Required': true,
+              'Privacy': UserProperties.Privacy.Private
+            },
+            'EmailToken': {
+              'Required': true,
+              'Privacy': UserProperties.Privacy.Secret,
+              'Access': 'Email',
+              'Sources': ['Auto'],
+              'Generator': function(Callback) {Callback(null, Uid(20));},
+              'Description': EmailTokenValidation
+            },
+            '_id': {
+              'Privacy': UserProperties.Privacy.Private,
+              'Access': 'System',
+              'Sources': ['MongoDB']
+            }}
+        );
+        Setup(UserSchema, Callback);
+    },
+    'tearDown': function(Callback) {
+        Context.Server.close(function() {
+            Context.DB.dropDatabase(function(Err, Result) {
+                Context.DB.close();
+                Callback();
+            });
+        });
+    },
+    'POST /Users': function(Test) {
+        Test.expect(0);
+        Test.done();
+    },
+    'PATCH /User/Self': function(Test) {
+        Test.expect(0);
+        Test.done();
+    },
+    'DELETE /User/Self ': function(Test) {
+        Test.expect(0);
+        Test.done();
+    },
+    'GET /User/Self': function(Test) {
+        Test.expect(0);
+        Test.done();
+    },
+    'PUT /Session/Self/User': function(Test) {
+        Test.expect(0);
+        Test.done();
+    },
+    'DELETE /Session/Self/User': function(Test) {
+        Test.expect(0);
+        Test.done();
+    },
+    'GET /Users/:Field/:ID/Count': function(Test) {
+        Test.expect(0);
+        Test.done();
+    },
+    'PUT /User/Self/Memberships/:Membership': function(Test) {
+        Test.expect(0);
+        Test.done();
+    },
+    'DELETE /User/Self/Memberships/:Membership': function(Test) {
+        Test.expect(0);
+        Test.done();
+    },
+    'POST /User/Self/Recovery/:SetField': function(Test) {
+        Test.expect(0);
+        Test.done();
+    },
+    'POST /User/:Field/:ID/Recovery/:SetField': function(Test) {
+        Test.expect(0);
+        Test.done();
+    },
+    'PATCH /User/:Field/:ID': function(Test) {
+        Test.expect(0);
+        Test.done();
+    },
+    'DELETE /User/:Field/:ID': function(Test) {
+        Test.expect(0);
+        Test.done();
+    },
+    'GET /User/:Field/:ID': function(Test) {
+        Test.expect(0);
+        Test.done();
+    },
+    'PUT /User/:Field/:ID/Memberships/:Membership': function(Test) {
+        Test.expect(0);
+        Test.done();
+    },
+    'DELETE /User/:Field/:ID/Memberships/:Membership': function(Test) {
+        Test.expect(0);
+        Test.done();
+    }
+}
